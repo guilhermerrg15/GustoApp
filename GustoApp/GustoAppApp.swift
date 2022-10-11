b22069a90d15a795e2b247a029f279f303cfe542
Guilherme Gomes <guilhermerrg15@gmail.com>
2022-10-11 16:24:29 -0300

    Initial Commit

---
+++ GustoApp/GustoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GustoAppApp.swift
+//  GustoApp
+//
+//  Created by Guilherme Gomes on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct GustoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
