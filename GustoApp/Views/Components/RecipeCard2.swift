47c6854a4c33f9dc5546e714fe8e2baec57d2709
Guilherme Gomes <guilhermerrg15@gmail.com>
2022-10-19 14:48:05 -0300

    Tela Home atualizada

--- GustoApp/Views/Components/RecipeCard2.swift
+++ GustoApp/Views/Components/RecipeCard2.swift
@@ -15,16 +15,18 @@ struct RecipeCard2: View {
             VStack{
                 RoundedRectangle(cornerRadius: 10, style: .continuous)
                     .fill(Color(.white))
-                    .frame(width: 210, height: 250, alignment: .center)
-                    .shadow(color: Color.black.opacity(0.3), radius: 15, x: 0, y: 30)
+                    .frame(width: 246, height: 273, alignment: .center)
+                    .shadow(color: Color.black.opacity(0.5), radius: 6, x: 0, y: 5)
                     .overlay(){
                         ZStack{
                             VStack{
                                 Text(recipe.name)
                                     .font(.headline)
                                     .foregroundColor(Color("ColorWine"))
-                                //.frame(maxWidth: 110)
-                                    .padding()
+                                    .multilineTextAlignment(.leading)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .padding(.leading, 10)
+                                    
                             }.padding(.top,120)
                             VStack{
                                 Spacer()
@@ -34,7 +36,7 @@ struct RecipeCard2: View {
                                         .foregroundColor(.gray)
                                     Text(recipe.recipeTime)
                                         .foregroundColor(.gray)
-                                        .font(Font.custom("Futura",size: 14))
+                                        .font(Font.custom("Futura",size: 13))
                                         .padding(.trailing)
                                 }.padding(.bottom,10)
                             }
@@ -44,16 +46,17 @@ struct RecipeCard2: View {
             }
             Image(recipe.image)
                 .resizable()
-                .frame(width: 160, height: 160, alignment: .top)
+                .frame(width: 222, height: 170, alignment: .top)
                 .background(LinearGradient(gradient: Gradient(colors: [Color.gray.opacity(0.3), Color(.gray)]), startPoint: .top, endPoint: .bottom))
                 .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+                .padding(.top,8)
         }
     }
     
     
     struct RecipeCard2_Previews: PreviewProvider {
         static var previews: some View {
-            RecipeCard2(recipe: Recipe.MediumRecipes[2])
+            RecipeCard2(recipe: Recipe.MediumRecipes[0])
         }
     }
 }
